21f36c7fe2e9c782a28250a00e298670ecc4f7b9
Alan-LR <alanlopes.r@gmail.com>
2022-11-28 20:49:49 -0300

    AULA - Criando nosso serviço POST e efetuando um cadastro de usuário

--- restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
+++ restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +50,11 @@ public class IndexController {
 	@GetMapping(value = "/" , produces = "application/json")
 	public ResponseEntity<List<Usuario>> buscarTodos(){
 		return new ResponseEntity<List<Usuario>>(usuarioRepository.findAll(), HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		return new ResponseEntity<Usuario>(usuarioRepository.save(usuario), HttpStatus.CREATED);
 	}
 	
 }
