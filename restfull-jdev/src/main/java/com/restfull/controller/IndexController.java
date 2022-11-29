79a95e2d7e28c392119b26ff1176488c7e035d68
Alan-LR <alanlopes.r@gmail.com>
2022-11-28 21:11:48 -0300

    AULA - Criando a atualização de cadastro com o PUT

--- restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
+++ restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,6 +56,11 @@ public class IndexController {
 	@PostMapping(value = "/", produces = "application/json")
 	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
 		return new ResponseEntity<Usuario>(usuarioRepository.save(usuario), HttpStatus.CREATED);
+	}
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar (@RequestBody Usuario usuario){
+		return new ResponseEntity<Usuario>(usuarioRepository.saveAndFlush(usuario), HttpStatus.OK);
 	}
 	
 }
