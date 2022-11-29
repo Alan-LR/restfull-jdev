268a1e237c36196684bd589f92d5af23e20a684e
Alan-LR <alanlopes.r@gmail.com>
2022-11-28 21:23:58 -0300

    AULA - Criando a remoção com DELETE

--- restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
+++ restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,4 +64,10 @@ public class IndexController {
 		return new ResponseEntity<Usuario>(usuarioRepository.saveAndFlush(usuario), HttpStatus.OK);
 	}
 	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public ResponseEntity<String> delete(@PathVariable Long id) {
+		usuarioRepository.deleteById(id);
+		return new ResponseEntity<String>("Usuario deletado", HttpStatus.OK);
+	}
+		
 }
