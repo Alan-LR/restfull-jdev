ad2208c5fda795ad26a50b995f894daff7c72b63
Alan-LR <alanlopes.r@gmail.com>
2022-11-30 15:25:29 -0300

    AULA - Cross Origin - Configuração em controler especifico

--- restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
+++ restfull-jdev/src/main/java/com/restfull/controller/IndexController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.restfull.jdev.model.Usuario;
 import com.restfull.jdev.repository.UsuarioRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
